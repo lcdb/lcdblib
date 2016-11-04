3b75b5ae591a265cab8991639d4c44e5ca8d1aab
daler <dalerr@niddk.nih.gov>
2016-11-04 15:52:48 -0400

    fix channels

--- travis-setup.sh
+++ travis-setup.sh
@@ -12,10 +12,10 @@ sudo bash Miniconda3-latest-${tag}-x86_64.sh -b -p /anaconda
 sudo chown -R $USER /anaconda
 export PATH=/anaconda/bin:$PATH
 
-# Add channels in the specified order.
-for channel in $(grep -v "^#" bioconda_utils/channel_order.txt); do
-    conda config --add channels $channel
-done
+conda config --add channels conda-forge
+conda config --add channels defaults
+conda config --add channels r
+conda config --add channels bioconda
 
 conda install -y --file requirements_dev.txt
 python setup.py install
