a672d2a05ee384ec1ea04f74f808097d30084da7
daler <dalerr@niddk.nih.gov>
2016-11-05 15:09:10 -0400

    simplify travis-setup

--- travis-setup.sh
+++ travis-setup.sh
@@ -1,21 +1,4 @@
 #!/bin/bash
 set -euo pipefail
-
-if [[ $TRAVIS_OS_NAME = "linux" ]]
-then
-    tag=Linux
-else
-    tag=MacOSX
-fi
-curl -O https://repo.continuum.io/miniconda/Miniconda3-latest-${tag}-x86_64.sh
-sudo bash Miniconda3-latest-${tag}-x86_64.sh -b -p /anaconda
-sudo chown -R $USER /anaconda
-export PATH=/anaconda/bin:$PATH
-
-conda config --add channels conda-forge
-conda config --add channels defaults
-conda config --add channels r
-conda config --add channels bioconda
-
 pip install -r requirements_dev.txt
 python setup.py install
