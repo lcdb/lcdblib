64dc0a2fa87b990320def7e77d87bab8a48dc356
daler <dalerr@niddk.nih.gov>
2016-11-15 16:36:36 -0500

    conda paths

--- travis-setup.sh
+++ travis-setup.sh
@@ -2,18 +2,9 @@
 set -euo pipefail
 set -x
 
-# Sets up travis-ci environment for testing bioconda-utils.
-
-if [[ $TRAVIS_OS_NAME = "linux" ]]
-then
-    tag=Linux
-else
-    tag=MacOSX
-fi
-curl -O https://repo.continuum.io/miniconda/Miniconda3-latest-${tag}-x86_64.sh
-sudo bash Miniconda3-latest-${tag}-x86_64.sh -b -p /anaconda
-sudo chown -R $USER /anaconda
-export PATH=/anaconda/bin:$PATH
+curl -O https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
+bash Miniconda3-latest-${tag}-x86_64.sh -b -p ~/anaconda
+export PATH=~/anaconda/bin:$PATH
 
 # Add channels in the specified order.
 conda config --add channels conda-forge
