2f1c1fb46a110a6f22d1355600e3f5a3d06cae88
Justin Fear <justin.m.fear@gmail.com>
2016-11-27 15:11:01 -0500

    Updates tests

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -1,45 +1,47 @@
-import unittest, pytest
-import os
-import sys
-import re
-import yaml
-
+import pytest
 import pandas as pd
-
 from lcdblib.snakemake import helpers
 
-HERE = os.path.dirname(os.path.realpath(__file__))
-sys.path.insert(0, os.path.join(HERE, '../'))
+@pytest.fixture(scope='session')
+def patterns():
+    patterns = {
+        'fastq':   'samples/{sample}/{sample}_R{N}.fastq.gz',
+        'cutadapt': 'samples/{sample}/{sample}_R{N}.cutadapt.fastq.gz',
+        'bam':     'samples/{sample}/{sample}.cutadapt.bam',
+    }
 
+    return patterns
 
-class TestHelperFunctions(unittest.TestCase):
+def test_fill_patterns_dict(patterns):
+        fill = dict(sample=['U1', 'U2', 'T1', 'T2'], N=[1, 2])
+        res = helpers.fill_patterns(patterns, fill)['fastq']
 
-    def setUp(self):
+        expected = [
+                    'samples/T1/T1_R1.fastq.gz',
+                    'samples/T1/T1_R2.fastq.gz',
+                    'samples/T2/T2_R1.fastq.gz',
+                    'samples/T2/T2_R2.fastq.gz',
+                    'samples/U1/U1_R1.fastq.gz',
+                    'samples/U1/U1_R2.fastq.gz',
+                    'samples/U2/U2_R1.fastq.gz',
+                    'samples/U2/U2_R2.fastq.gz',
+                   ]
 
-        # Example config
-        self.config = {'sampletable': 'pasilla_sampletable.tsv',
-                       'sample_dir': 'pasilla',
-                       'assembly': 'dm6',
-                       'data_dir': '/data/LCDB/references-test',
-                       'shell_prefix': "source lcdb-workflows.bashrc; ",
-                       'rules': {
-                           'cutadapt': {
-                               'params': {
-                                   'extra': '-a file:adapters.fa --quality-cutoff 20 --minimum-length 25 --overlap 10'
-                               },
-                               'threads': 10
-                           }
-                       }
-                       }
+        assert expected == sorted(res)
 
-        # Build basic sample table
-        from tempfile import mktemp
-        self.tempFile = mktemp()
+def test_fill_patterns_df(patterns):
+    fill = pd.DataFrame({
+        'sample': ['U1', 'U2', 'T1', 'T2'],
+        'N': [1, 2, 3, 4],
+        })
 
-        self.df = pd.DataFrame({'sampleID': ['U1', 'U2', 'T1', 'T2'], 'sex': ['M', 'M', 'M', 'M'], 'rep': [1, 2, 1, 2], })
-        self.df.set_index('sampleID', inplace=True)
-        self.df.to_csv(self.tempFile, sep='\t')
+    res = helpers.fill_patterns(patterns, fill)['fastq']
 
-    def tearDown(self):
-        os.remove(self.tempFile)
+    expected = [
+                'samples/T1/T1_R3.fastq.gz',
+                'samples/T2/T2_R4.fastq.gz',
+                'samples/U1/U1_R1.fastq.gz',
+                'samples/U2/U2_R2.fastq.gz',
+               ]
 
+    assert expected == sorted(res)
