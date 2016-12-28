9ed86414028befebc7643e0ac2f4172f0c17f8bf
Ryan Dale <dalerr@niddk.nih.gov>
2016-12-28 14:39:16 -0500

    add anaconda upload

--- conda-build-and-upload.sh
+++ conda-build-and-upload.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+
+# Always build with conda to test any build issues, but only upload if we're on
+# master branch and it's not a pull request.
 export LCDBLIB_VERSION=$(./_version.py version)
 export LCDBLIB_BUILD=$(./_version.py build)
 conda build conda-recipe
+
+if [[ $TRAVIS_BRANCH = "master" && $TRAVIS_PULL_REQUEST = "false" ]]; then
+  conda install anaconda
+  anaconda upload \
+    -t $ANACONDA_TOKEN \
+    /home/travis/anaconda/conda-bld/linux-64/lcdblib-${LCDBLIB_VERSION}-${LCDBLIB_BUILD}.tar.bz2
+fi
