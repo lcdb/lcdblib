e52bd1061033e03ee373f091f2a1bb4e33074bce
Ryan Dale <dalerr@niddk.nih.gov>
2016-12-29 09:59:27 -0500

    fix arg order

--- conda-build-and-upload.sh
+++ conda-build-and-upload.sh
@@ -8,7 +8,8 @@ conda build conda-recipe
 
 if [[ $TRAVIS_BRANCH = "master" && $TRAVIS_PULL_REQUEST = "false" ]]; then
   conda install anaconda-client -y
-  anaconda upload \
+  anaconda \
     -t $ANACONDA_TOKEN \
+    upload \
     /home/travis/anaconda/conda-bld/linux-64/lcdblib-${LCDBLIB_VERSION}-${LCDBLIB_BUILD}.tar.bz2
 fi
