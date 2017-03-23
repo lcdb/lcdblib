70e8abeef9d4cc8cd54b5ac4e60890a1af22c4dd
Ryan Dale <dalerr@niddk.nih.gov>
2017-03-23 13:58:03 -0400

    move pinned samtools to separate reqs file

--- travis-setup.sh
+++ travis-setup.sh
@@ -12,6 +12,7 @@ conda config --add channels r
 conda config --add channels bioconda
 
 conda install -y python=3.5
+conda install -y --file conda-requirements.txt
 conda install -y --file requirements.txt
 
 # we also want conda-build to build a conda package
