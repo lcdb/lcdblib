3d26feff510d93dd220fc8aefb906ada06854e97
Ryan Dale <dalerr@niddk.nih.gov>
2017-06-08 12:26:43 -0400

    rm theme from extensions

--- docs/conf.py
+++ docs/conf.py
@@ -42,7 +42,7 @@ import lcdblib
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.autosummary',
               'sphinx.ext.viewcode', 'sphinx.ext.napoleon',
-              'guzzle_sphinx_theme']
+            ]
 napolean_numpy_docstring = True
 
 
