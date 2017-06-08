007175bf442e432d1908ae1c162fe1a566913865
Ryan Dale <dalerr@niddk.nih.gov>
2017-06-08 12:16:58 -0400

    back to requirements.txt

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = [i.strip() for i in open('conda-requirements.txt').readlines()]
+requirements = [i.strip() for i in open('requirements.txt').readlines()]
 
 setup(
     name='lcdblib',
