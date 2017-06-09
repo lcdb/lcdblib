f4a5a4f45305d77796fb7d74bb92ab7b996e89e1
Ryan Dale <dalerr@niddk.nih.gov>
2017-06-09 10:38:29 -0400

    update version style

--- lcdblib/_version.py
+++ lcdblib/_version.py
@@ -39,7 +39,7 @@ def get_config():
     # _version.py
     cfg = VersioneerConfig()
     cfg.VCS = "git"
-    cfg.style = "pep440"
+    cfg.style = "pep440-pre"
     cfg.tag_prefix = ""
     cfg.parentdir_prefix = "None"
     cfg.versionfile_source = "lcdblib/_version.py"
