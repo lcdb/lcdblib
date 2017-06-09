36c484e7c7e4608b4235de23fb1b6f3d79eb84e3
Ryan Dale <dalerr@niddk.nih.gov>
2017-06-09 10:38:56 -0400

    add __conda_{version,build}__ vars

--- lcdblib/__init__.py
+++ lcdblib/__init__.py
@@ -1,3 +1,9 @@
 from ._version import get_versions
 __version__ = get_versions()['version']
 del get_versions
+
+# The following assumes versioneer was configured with the "pep440-pre" style
+# in setup.cfg
+__conda_version__, __conda_build__ = __version__.split('.dev')
+if __conda_build__ == "":
+    __conda_build__ = '0'
