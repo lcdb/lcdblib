569e53cf2e60fd7b28bdf2df88b21c378339ef5d
Ryan Dale <dalerr@niddk.nih.gov>
2017-06-09 10:39:10 -0400

    conda build script uses new versioning system

--- conda-build-and-upload.sh
+++ conda-build-and-upload.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-# Always build with conda to test any build issues, but only upload if we're on
-# master branch and it's not a pull request.
-export LCDBLIB_VERSION=$(./_version.py version)
-export LCDBLIB_BUILD=$(./_version.py build)
+export LCDBLIB_VERSION=$(python -c 'from lcdblib import __conda_version__; print(__conda_version__)')
+export LCDBLIB_BUILD=$(python -c 'from lcdblib import __conda_build__; print(__conda_build__)')
+
 conda build conda-recipe
 
+# Always build with conda to test any build issues, but only upload if we're on
+# master branch and it's not a pull request.
 if [[ $TRAVIS_BRANCH = "master" && $TRAVIS_PULL_REQUEST = "false" ]]; then
   conda install anaconda-client -y
   anaconda \
