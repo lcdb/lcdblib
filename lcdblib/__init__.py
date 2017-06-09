2763e269cf827c651750c8f01298dad710a757f6
Ryan Dale <dalerr@niddk.nih.gov>
2017-06-09 10:22:47 -0400

    rm old version stuff

--- lcdblib/__init__.py
+++ lcdblib/__init__.py
@@ -1,8 +1,3 @@
-# -*- coding: utf-8 -*-
-from ._version import get_version
-version, build = get_version()
-__version__ = '{0}.{1}'.format(version, build)
-
 from ._version import get_versions
 __version__ = get_versions()['version']
 del get_versions
