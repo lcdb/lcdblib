9da295459e873e2604172b457fcc74bcdf098b28
Ryan Dale <dalerr@niddk.nih.gov>
2017-07-04 11:09:06 -0400

    build recipe for multiple versions of python

--- ci/conda-build-and-upload.sh
+++ ci/conda-build-and-upload.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
-set -euo pipefail
+set -eo pipefail
 
 export LCDBLIB_VERSION=$(python -c 'from lcdblib import __conda_version__; print(__conda_version__)')
 export LCDBLIB_BUILD=$(python -c 'from lcdblib import __conda_build__; print(__conda_build__)')
 
-conda build conda-recipe
-
-# Always build with conda to test any build issues, but only upload if we're on
-# master branch and it's not a pull request.
 if [[ $TRAVIS_BRANCH = "master" && $TRAVIS_PULL_REQUEST = "false" ]]; then
   conda install anaconda-client -y
-  anaconda \
-    -t $ANACONDA_TOKEN \
-    upload \
-    -u lcdb \
-    $(conda build --output conda-recipe)
 fi
+
+# Build packages for all supported versions of Python. If we're on the master
+# branch and on Travis-CI, then also upload the built package.
+for PY in 35 36; do
+    CONDA_PY=$PY conda build conda-recipe
+    if [[ $TRAVIS_BRANCH = "master" && $TRAVIS_PULL_REQUEST = "false" ]]; then
+      anaconda \
+        -t $ANACONDA_TOKEN \
+        upload \
+        -u lcdb \
+        $(CONDA_PY=$PY conda build --output conda-recipe)
+    fi
+done
