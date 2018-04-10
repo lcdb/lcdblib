02c329c34229cd7cdd12fe4187057b823ba7121b
Travis CI <lcdb@users.noreply.github.com>
2018-04-10 20:21:13 +0000

    Updated docs to commit 8273304ffe7eb0270d716530169c04ecf4022dd6.

---
+++ _static/documentation_options.js
@@ -0,0 +1,9 @@
+var DOCUMENTATION_OPTIONS = {
+    URL_ROOT: '',
+    VERSION: '0.1.2.post.dev62',
+    LANGUAGE: 'None',
+    COLLAPSE_INDEX: false,
+    FILE_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt'
+};
