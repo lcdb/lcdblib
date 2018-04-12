0948daea0ab1d9cd6e333a8b08e437fbcb8b2dcd
Ryan Dale <dalerr@niddk.nih.gov>
2018-04-12 10:20:02 -0400

    support for versioning if no subsequent commits made after tag

--- lcdblib/__init__.py
+++ lcdblib/__init__.py
@@ -4,6 +4,9 @@ del get_versions
 
 # The following assumes versioneer was configured with the "pep440-pre" style
 # in setup.cfg
-__conda_version__, __conda_build__ = __version__.split('.dev')
-if __conda_build__ == "":
+toks = __version__.split('.dev')
+if len(toks) == 2:
+    __conda_version__, __conda_build__ = toks
+else:
     __conda_build__ = '0'
+    __conda_version__ = toks[0]
